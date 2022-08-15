8ebd60aa3b1507f2b02c99e2ff2a314dfb3f99f8
Alfi Mostak <alfimostak99@gmail.com>
2022-08-16 00:01:59 +0600

    last_commit_alfi

--- src/main/java/com/volunteer/web/UserRegistrationController.java
+++ src/main/java/com/volunteer/web/UserRegistrationController.java
@@ -1,5 +1,6 @@
 package com.volunteer.web;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,8 +36,11 @@ public class UserRegistrationController {
 	
 	@PostMapping
 	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+		BCryptPasswordEncoder encoder  = new BCryptPasswordEncoder();
+		String encodedPassword = encoder.encode(registrationDto.getPassword());
+		registrationDto.setPassword(encodedPassword);
 		userService.save(registrationDto);
 		return "redirect:/registration?success";
 	}
-
+ 
 }
