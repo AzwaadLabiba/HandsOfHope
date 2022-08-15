37e16e2b69e11b554d04fe767eef718998f3e6b7
Alfi Mostak <alfimostak99@gmail.com>
2022-08-16 00:04:32 +0600

    last_commit_alfi:login

--- src/main/java/com/volunteer/repositories/UserRepository.java
+++ src/main/java/com/volunteer/repositories/UserRepository.java
@@ -1,11 +1,13 @@
 package com.volunteer.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.volunteer.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
-	
+	@Query("SELECT u FROm User u WHERE u.email = ?1")
+	User findByEmail(String email);
 }
