cfa2995212037181e6908cfb4813465a4b516fdd
Alfi Mostak <alfimostak99@gmail.com>
2022-08-16 00:05:55 +0600

    last_commit_alfi:userDetails

---
+++ src/main/java/com/volunteer/models/CustomUserDetails.java
@@ -0,0 +1,71 @@
+package com.volunteer.models;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails implements UserDetails {
+
+	
+	private User user;
+	
+	
+	public CustomUserDetails(User user) {
+		
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return user.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return  true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	public String getFullname() {
+		return this.user.getF_name() + " " + this.user.getL_name();
+	}
+	public void  setF_name(String f_name) {
+		this.user.setF_name(f_name);
+	}
+public void  setL_name(String l_name) {
+	this.user.setL_name(l_name);
+	}
+
+}
